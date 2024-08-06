5384b255b321ec6afbc4f4f3bcd5874500d48222
Ivan Donchenko <ivan.don4enko@gmail.com>
2024-08-06 21:14:58 +0300

    excluded unnessesary landscape views in subWorkspaces

--- dsl/scripts/landscape.groovy
+++ dsl/scripts/landscape.groovy
@@ -5,23 +5,41 @@ import com.structurizr.configuration.WorkspaceScope
 
 @Field static final STRUCTURIZR_ONPREMISES_URL = 'http://structurizr.solvo.ru'
 @Field static final ADMIN_API_KEY_PLAINTEXT = 'CozyPlace-36'
+  
+if(workspace.WorkspaceScope!="Landscape" ) break  //We only run this script for higher level workspace
 
+/**
+*   First find all existing System's workspaces and map their url's to the corresponding landscape's elements 
+*   
+**/
 
-def workspaces = getWorkspaces([2, 3, 4])
-workspaces.each { subWorkspace ->
-    def softwareSystem = subWorkspace.model.softwareSystems.find { !it.containers.isEmpty() }
-    if (softwareSystem) {
+//def workspaces = getWorkspaces([2, 3, 4])
+
+def LandscapeWorkspace = workspace
+def SoftwareSystemWorkspaces = getWorkspaces()
+
+SoftwareSystemWorkspaces.each { subWorkspace ->
+    def softwareSystemFromSubWorkspace = subWorkspace.model.softwareSystems.find { !it.containers.isEmpty() }
+    if (softwareSystemFromSubWorkspace) {
+        LandscapeWorkspace.model.elements.find {
+             getElementKey(it) == softwareSystemFromSubWorkspace.properties['structurizr.dsl.identifier'] 
+        }.setUrl("{workspace:${it.id}}/diagrams")
+
         findElementByKey(workspace, getElementKey(softwareSystem))
-                .setUrl("{workspace:${subWorkspace.id}}/diagrams")
+                .setUrl("{workspace:${it.id}}/diagrams")
     }
 }
 
 def view = workspace.views.createSystemLandscapeView("solvo-products", "Экосистема продуктов Solvo");
 view.addAllElements();
 
-static List getWorkspaces(List workspaceIds) {
+//static List getWorkspaces(List workspaceIds) {
+static List getWorkspaces() {
     def adminApiClient = new AdminApiClient("${STRUCTURIZR_ONPREMISES_URL}/api", '', ADMIN_API_KEY_PLAINTEXT)
-    def workspaces = adminApiClient.workspaces.findAll { it.id in workspaceIds }
+   // def workspaces = adminApiClient.workspaces.findAll { it.id in workspaceIds }
+    
+    def workspaces = adminApiClient.workspaces.findAll{ it.WorkspaceScope="SoftwareSystem" }
+
     return workspaces.collect {
         def workspaceApiClient = new WorkspaceApiClient("${STRUCTURIZR_ONPREMISES_URL}/api", it.apiKey, it.apiSecret)
         workspaceApiClient.workspaceArchiveLocation = null
